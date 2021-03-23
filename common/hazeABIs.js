6272618f2834128401d02c923a10120f540653c2
Mona Lisa <monalisa1503@protonmail.com>
2021-03-23 22:17:37 +0000

    mvp

---
+++ common/hazeABIs.js
@@ -0,0 +1,15 @@
+const ERC20 = require('../../contracts/ERC20.abi.json');
+const SwapPair = require('../../contracts/SwapPair.abi.json');
+const SwapRouter = require('../../contracts/SwapRouter.abi.json');
+const HAZE = require('../../contracts/HAZE.abi.json');
+const SHAZE = require('../../contracts/SHAZE.abi.json');
+const Hazer = require('../../contracts/Hazer.abi.json');
+const DepositReward = require('../../contracts/DepositRewardV2.abi.json');
+const LiquidityMiningReward = require('../../contracts/LiquidityMiningReward.abi.json');
+const TokenDistributor = require('../../contracts/TokenDistributor.abi.json');
+
+
+const HAZE_ABIS = { ERC20, SwapPair, SwapRouter, HAZE, SHAZE, Hazer, DepositReward, LiquidityMiningReward, TokenDistributor };
+
+
+module.exports = HAZE_ABIS;
---
+++ index.js
@@ -0,0 +1,12 @@
+const hazeAddresses56 = require('./56/hazeAddresses');
+const HAZE_ABIS = require('./common/hazeABIs');
+const MERKLE_TREE_HEIGHT = 31;
+
+const HAZE_ADDRESSES = {
+  '56': hazeAddresses56
+}
+
+
+module.exports = HAZE_ADDRESSES;
+module.exports = HAZE_ABIS;
+module.exports = MERKLE_TREE_HEIGHT;
