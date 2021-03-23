c36a935cd73d9306df6b7956ccc39adabda95f20
Mona Lisa <monalisa1503@protonmail.com>
2021-03-23 22:50:39 +0000

    add haze property file

---
+++ 56/hazeProperties.js
@@ -0,0 +1,16 @@
+
+const HAZE_PROPERTIES = {
+  'BUSD': {
+    '1000': {
+      'merkleTreeHeight': 31
+    },
+    '10000': {
+      'merkleTreeHeight': 31
+    },
+    '100000': {
+      'merkleTreeHeight': 31
+    }
+  }
+};
+
+module.exports = HAZE_PROPERTIES;
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const hazeAddresses56 = require('./56/hazeAddresses');
+const hazeProperties56 = require('./56/hazeProperties');
 const HAZE_ABIS = require('./common/hazeABIs');
-const MERKLE_TREE_HEIGHT = 31;
 
 const HAZE_ADDRESSES = {
   '56': hazeAddresses56
 }
 
+const HAZE_PROPERTIES = {
+  '56': hazeProperties56
+}
+
 
 module.exports = HAZE_ADDRESSES;
+module.exports = HAZE_PROPERTIES;
 module.exports = HAZE_ABIS;
-module.exports = MERKLE_TREE_HEIGHT;
