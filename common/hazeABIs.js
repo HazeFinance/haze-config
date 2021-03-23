53fdcf1c88ccd3005bf1c25dbc68c2f6ef5b9e78
Mona Lisa <monalisa1503@protonmail.com>
2021-03-23 23:10:13 +0000

    fix ABI file path

--- common/hazeABIs.js
+++ common/hazeABIs.js
@@ -1,12 +1,12 @@
-const ERC20 = require('../../contracts/ERC20.abi.json');
-const SwapPair = require('../../contracts/SwapPair.abi.json');
-const SwapRouter = require('../../contracts/SwapRouter.abi.json');
-const HAZE = require('../../contracts/HAZE.abi.json');
-const SHAZE = require('../../contracts/SHAZE.abi.json');
-const Hazer = require('../../contracts/Hazer.abi.json');
-const DepositReward = require('../../contracts/DepositRewardV2.abi.json');
-const LiquidityMiningReward = require('../../contracts/LiquidityMiningReward.abi.json');
-const TokenDistributor = require('../../contracts/TokenDistributor.abi.json');
+const ERC20 = require('./ABIs/ERC20.abi.json');
+const SwapPair = require('./ABIs/SwapPair.abi.json');
+const SwapRouter = require('./ABIs/SwapRouter.abi.json');
+const HAZE = require('./ABIs/HAZE.abi.json');
+const SHAZE = require('./ABIs/SHAZE.abi.json');
+const Hazer = require('./ABIs/Hazer.abi.json');
+const DepositReward = require('./ABIs/DepositRewardV2.abi.json');
+const LiquidityMiningReward = require('./ABIs/LiquidityMiningReward.abi.json');
+const TokenDistributor = require('./ABIs/TokenDistributor.abi.json');
 
 
 const HAZE_COMMON_ABIS = { ERC20, SwapPair, SwapRouter, HAZE, SHAZE, Hazer, DepositReward, LiquidityMiningReward, TokenDistributor };
