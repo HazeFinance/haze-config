7d7118e80f6f3ffd1e1053532f48961f9424bd71
Mona Lisa <monalisa1503@protonmail.com>
2021-04-09 20:39:43 +0100

    add contractCreationBlock

--- 56/hazeProperties.js
+++ 56/hazeProperties.js
@@ -2,24 +2,30 @@
 const HAZE_PROPERTIES = {
   'BUSD': {
     '1000': {
-      'merkleTreeHeight': 31
+      'merkleTreeHeight': 31,
+      'contractCreationBlock': 5282663
     },
     '10000': {
-      'merkleTreeHeight': 31
+      'merkleTreeHeight': 31,
+      'contractCreationBlock': 5282672
     },
     '100000': {
-      'merkleTreeHeight': 31
+      'merkleTreeHeight': 31,
+      'contractCreationBlock': 5282713
     }
   },
   'BNB': {
     '1': {
-      'merkleTreeHeight': 31
+      'merkleTreeHeight': 31,
+      'contractCreationBlock': 6420413
     },
     '10': {
-      'merkleTreeHeight': 31
+      'merkleTreeHeight': 31,
+      'contractCreationBlock': 6421547
     },
     '100': {
-      'merkleTreeHeight': 31
+      'merkleTreeHeight': 31,
+      'contractCreationBlock': 6421555
     }
   }
 };
